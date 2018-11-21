b3db6042cc8d129a4a0a15668aabe50fcc892798
coe18161676 <coe18161676@CLCLRCAC08.belfastmet.int>
2018-11-21 09:15:40 +0000

    Code Comment

--- 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbcontroller/controller/Playground.java
+++ 03-03-SpringBootControllers/src/main/java/uk/ac/belfastmet/sbcontroller/controller/Playground.java
@@ -1,7 +1,7 @@
 package uk.ac.belfastmet.sbcontroller.controller;
 
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
