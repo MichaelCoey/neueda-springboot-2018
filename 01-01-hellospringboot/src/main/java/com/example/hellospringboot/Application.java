1dc6e213fe6607973327d624c0f32e6ad64a2bbe
Michael Coey (COE18161676) <COE18161676@belfastmet.ac.uk>
2018-11-23 10:19:40 +0000

    We added a comment to the first project application code

--- 01-01-hellospringboot/src/main/java/com/example/hellospringboot/Application.java
+++ 01-01-hellospringboot/src/main/java/com/example/hellospringboot/Application.java
@@ -13,6 +13,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
+		//This is a change for git to find
 		logger.info("Hello Spring Boot");
 	}
 }
